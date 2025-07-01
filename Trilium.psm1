f36d3be96010ce4fa00d7be505a483260179dcd2
Patrick Morris <patrickthomasmorris@gmail.com>
2025-07-01 09:24:18 -0500

    changed to lowercase to support linux

--- Trilium.psm1
+++ Trilium.psm1
@@ -1,10 +1,10 @@
 # Dot source all public functions
-Get-ChildItem -Path "$PSScriptRoot\Public\*.ps1" -Recurse | ForEach-Object {
+Get-ChildItem -Path "$PSScriptRoot\public\*.ps1" -Recurse | ForEach-Object {
     . $_.FullName
 }
 
 # Dot source all private/internal functions
-Get-ChildItem -Path "$PSScriptRoot\Private\*.ps1" -Recurse | ForEach-Object {
+Get-ChildItem -Path "$PSScriptRoot\private\*.ps1" -Recurse | ForEach-Object {
     . $_.FullName
     Export-ModuleMember -Function $_.BaseName
 }
